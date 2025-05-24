03998de6933bdb94d766ab1e574926d610204b75
HuyTran102 <vuthiquynhchi72@gmail.com>
2025-05-24 22:44:56 +0700

    add constraint layout for dietary and diary item

--- app/src/main/java/com/huytran/goodlife/pages/dietary/DiaryItemDataActivity.java
+++ app/src/main/java/com/huytran/goodlife/pages/dietary/DiaryItemDataActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -36,7 +37,8 @@ import java.util.Objects;
 public class DiaryItemDataActivity extends AppCompatActivity {
     private TextView viewItemName, itemKcalo, itemProtein, itemLipid, itemGlucid, itemUnitType, itemUnitName;
     private TextInputEditText itemAmount;
-    private Button backButton, saveButton;
+    private ImageButton backButton;
+    private Button saveButton;
     private ImageView itemImage;
     private String glucidValue , lipidValue, proteinValue, kcalValue, name;
     private FirebaseFirestore firebaseFirestore;
