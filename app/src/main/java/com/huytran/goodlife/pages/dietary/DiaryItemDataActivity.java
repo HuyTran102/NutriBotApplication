2fd1718da39ee6d946314e225ece2da45f3d482a
HuyTran102 <vuthiquynhchi72@gmail.com>
2025-05-24 21:11:26 +0700

    make DiaryItemDataActivity.java status bar transparent

--- app/src/main/java/com/huytran/goodlife/pages/dietary/DiaryItemDataActivity.java
+++ app/src/main/java/com/huytran/goodlife/pages/dietary/DiaryItemDataActivity.java
@@ -11,6 +11,8 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -43,6 +45,19 @@ public class DiaryItemDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diary_item_data);
+
+        // Make status bar fully transparent
+        Window window = getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(getResources().getColor(android.R.color.transparent));
+
+        // Set the layout to extend into the status bar
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+
 
         backButton = findViewById(R.id.back_button);
         itemAmount = findViewById(R.id.item_amount);
