53a7b92bc145767166936f47023ed244964425f7
HuyTran102 <vuthiquynhchi72@gmail.com>
2025-05-30 16:02:06 +0700

    fix some small issue

--- app/src/main/java/com/huytran/goodlife/pages/tracking_diagram/TrackingDiagramActivity.java
+++ app/src/main/java/com/huytran/goodlife/pages/tracking_diagram/TrackingDiagramActivity.java
@@ -18,6 +18,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
@@ -122,66 +123,173 @@ public class TrackingDiagramActivity extends AppCompatActivity {
     }
 
     private void setupInnerChart() {
-        firebaseFirestore.collection("GoodLife")
+        // Task 1: Lấy dữ liệu từ Firestore cho "Dinh dưỡng"
+        Task<QuerySnapshot> nutritionTask = firebaseFirestore.collection("GoodLife")
+                .document(name).collection("Dinh dưỡng")
+                .get();
+
+        // Task 2: Lấy dữ liệu từ Firebase Realtime Database
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("user");
+        Query userDatabase = reference.orderByChild("name").equalTo(name);
+        TaskCompletionSource<Void> realtimeDatabaseTask = new TaskCompletionSource<>();
+
+        userDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String gender = snapshot.child(name).child("gender").getValue(String.class);
+                String date = snapshot.child(name).child("date_of_birth").getValue(String.class);
+
+                String[] birth = date.split("/");
+                String year_of_birth = birth[2];
+
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+
+                int age = year - Integer.parseInt(year_of_birth);
+
+                if (gender.equals("Nam")) {
+                    if (10 <= age && age <= 11) {
+                        recommendEnergy = 1900;
+                    } else if (12 <= age && age <= 14) {
+                        recommendEnergy = 2200;
+                    } else {
+                        recommendEnergy = 2500;
+                    }
+                } else {
+                    if (10 <= age && age <= 11) {
+                        recommendEnergy = 1750;
+                    } else if (12 <= age && age <= 14) {
+                        recommendEnergy = 2050;
+                    } else {
+                        recommendEnergy = 2100;
+                    }
+                }
+
+                // Hoàn thành task khi dữ liệu từ Realtime Database được lấy
+                realtimeDatabaseTask.setResult(null);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                realtimeDatabaseTask.setException(error.toException());
+            }
+        });
+
+        // Get the current date
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH) + 1; // tháng 1 bắt đầu từ 0 trong Calendar
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        // Task 3: Lấy dữ liệu từ Firestore cho "Hoạt động thể lực"
+        Task<QuerySnapshot> activityTask = firebaseFirestore.collection("GoodLife")
+                .document(name).collection("Hoạt động thể lực")
+                .whereEqualTo("year", String.valueOf(year))
+                .whereEqualTo("month", String.valueOf(month))
+                .whereEqualTo("day", String.valueOf(day))
+                .get();
+
+        // Task 4: Lấy dữ liệu từ Firestore cho "Nhật kí"
+        Task<QuerySnapshot> diaryTask = firebaseFirestore.collection("GoodLife")
                 .document(name)
                 .collection("Nhật kí")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            // Loop through all documents
-                            if (!task.getResult().isEmpty()) {
+                .whereEqualTo("year", String.valueOf(year))
+                .whereEqualTo("month", String.valueOf(month))
+                .whereEqualTo("day", String.valueOf(day))
+                .get();
 
-                                double sum_protein = 0, sum_lipid = 0, sum_glucid = 0;
-                                int sumKcal = 0;
+        // Chờ cho tất cả các Task hoàn thành
+        Tasks.whenAll(nutritionTask, realtimeDatabaseTask.getTask(), activityTask, diaryTask)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        // Task 1: Xử lý kết quả của Nutrition task
+                        QuerySnapshot nutritionResult = nutritionTask.getResult();
+                        if (nutritionResult != null) {
+                            for (QueryDocumentSnapshot document : nutritionResult) {
+                                if (document.getString("userHeight") != null
+                                        && document.getString("userWeight") != null
+                                        && document.getString("userRecommendHeight") != null
+                                        && document.getString("userRecommendWeight") != null) {
+                                    try {
+                                        actualHeight = Double.parseDouble(document.getString("userHeight"));
+                                    } catch (Exception e) {
+                                        actualHeight = 0.0;
+                                    }
 
-                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                    int day = Integer.parseInt(document.getString("day")), month = Integer.parseInt(document.getString("month")), year = Integer.parseInt(document.getString("year"));
-                                    if (LocalDate.now().getDayOfMonth() == day && LocalDate.now().getYear() == year
-                                            && LocalDate.now().getMonthValue() == month) {
-                                        sumKcal += Integer.parseInt(document.getString("kcal"));
-                                        sum_protein += Double.parseDouble(document.getString("protein"));
-                                        sum_lipid += Double.parseDouble(document.getString("lipid"));
-                                        sum_glucid += Double.parseDouble(document.getString("glucid"));
+                                    try {
+                                        actualWeight = Double.parseDouble(document.getString("userWeight"));
+                                    } catch (Exception e) {
+                                        actualWeight = 0.0;
+                                    }
+
+                                    try {
+                                        recommendHeight = Double.parseDouble(document.getString("userRecommendHeight"));
+                                    } catch (Exception e) {
+                                        recommendHeight = 0.0;
+                                    }
+
+                                    try {
+                                        recommendWeight = Double.parseDouble(document.getString("userRecommendWeight"));
+                                    } catch (Exception e) {
+                                        recommendWeight = 0.0;
                                     }
                                 }
-
-
-                                double protein_per = sum_protein / sumKcal * 100;
-                                double lipid_per = sum_lipid / sumKcal * 100;
-                                double glucid_per = sum_glucid / sumKcal * 100;
-
-                                ArrayList<PieEntry> entries = new ArrayList<>();
-                                entries.add(new PieEntry((float) glucid_per, "Bột"));
-                                entries.add(new PieEntry((float) protein_per, "Đạm"));
-                                entries.add(new PieEntry((float) lipid_per, "Béo"));
-
-                                PieDataSet dataSet = new PieDataSet(entries, "");
-                                dataSet.setColors(
-                                        Color.parseColor("#2E7D32"),  // Leaf Green
-                                        Color.parseColor("#CDDC39"),  // Lime Green
-                                        Color.parseColor("#81C784"),  // Fresh Green
-                                        Color.parseColor("#FFEB3B"),  // Bright Yellow
-                                        Color.parseColor("#FFD54F"),  // Light Amber
-                                        Color.parseColor("#9CCC65")   // Avocado Green
-                                );
-                                dataSet.setValueTextSize(10f);
-                                dataSet.setValueTextColor(getResources().getColor(R.color.text));
-                                dataSet.setDrawValues(true);
-
-                                PieData data = new PieData(dataSet);
-                                innerChart.setData(data);
-                                innerChart.setHoleRadius(40f);
-                                innerChart.setTransparentCircleRadius(40f);
-                                innerChart.getLegend().setEnabled(false);
-                                innerChart.getDescription().setEnabled(false);
-                                innerChart.setExtraBottomOffset(18f);
-                                innerChart.invalidate();
                             }
-                        } else {
-                            Log.w("Firestore", "Error getting documents", task.getException());
                         }
+
+                        recommendEnergy = recommendWeight * 24 * 1.5;
+
+                        // Task 3: Xử lý kết quả của Activity task
+                        QuerySnapshot activityResult = activityTask.getResult();
+                        if (activityResult != null) {
+                            double total_sum = 0;
+                            for (QueryDocumentSnapshot document : activityResult) {
+                                String amount = document.getString("userUsedEnergy");
+                                if (amount != null && !amount.isEmpty()) {
+                                    try {
+                                        total_sum += Double.parseDouble(amount);
+                                    } catch (NumberFormatException e) {
+                                        Log.w("Firestore", "Error parsing used energy", e);
+                                    }
+                                }
+                            }
+                            usedEnergy = total_sum;
+
+                            recommendEnergy = recommendWeight * 24 * 1.5 + usedEnergy;
+                        }
+
+                        double protein = (recommendEnergy * 0.2) / 4;
+                        double lipid = (recommendEnergy * 0.2) / 9;
+                        double glucid = (recommendEnergy * 0.6) / 4;
+
+                        ArrayList<PieEntry> entries = new ArrayList<>();
+                        entries.add(new PieEntry((float) glucid, "Bột"));
+                        entries.add(new PieEntry((float) protein, "Đạm"));
+                        entries.add(new PieEntry((float) lipid, "Béo"));
+
+                        PieDataSet dataSet = new PieDataSet(entries, "");
+                        dataSet.setColors(
+                                Color.parseColor("#EBD405"),  // Bright Yellow
+                                Color.parseColor("#E8A527"),  // Light Amber
+                                Color.parseColor("#82BF3B")   // Avocado Green
+                        );
+                        dataSet.setValueTextSize(10f);
+                        dataSet.setValueTextColor(getResources().getColor(R.color.text));
+                        dataSet.setDrawValues(true);
+
+                        PieData data = new PieData(dataSet);
+                        innerChart.setData(data);
+                        innerChart.setHoleRadius(0f);
+                        innerChart.setTransparentCircleRadius(0f);
+                        innerChart.getLegend().setEnabled(false);
+                        innerChart.getDescription().setEnabled(false);
+                        innerChart.setBackgroundColor(Color.TRANSPARENT);
+                        innerChart.setExtraBottomOffset(18f);
+                        innerChart.invalidate();
+
+                        Log.d("Firestore", "All tasks completed successfully");
+                    } else {
+                        Log.w("Firestore", "Error completing tasks", task.getException());
                     }
                 });
     }
@@ -239,9 +347,15 @@ public class TrackingDiagramActivity extends AppCompatActivity {
                                 pieChart.setData(pieData);
                                 pieChart.setUsePercentValues(true);
                                 pieChart.getDescription().setEnabled(false);
+                                Legend legend = pieChart.getLegend();
+                                legend.setEnabled(true);
+                                legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM); // căn dưới
+                                legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER); // căn giữa
+                                legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+                                legend.setDrawInside(false);
                                 pieChart.animateY(1000);
-                                pieChart.setHoleRadius(60f);
-                                pieChart.setTransparentCircleRadius(60f);
+                                pieChart.setHoleRadius(40f);
+                                pieChart.setTransparentCircleRadius(53f);
                                 pieChart.invalidate();
                             }
                         } else {
@@ -494,6 +608,12 @@ public class TrackingDiagramActivity extends AppCompatActivity {
                     dataSet2.setDrawValues(false);
 
                     LineData lineData = new LineData(dataSet1, dataSet2);
+                    Legend legend = lineChart.getLegend();
+                    legend.setEnabled(true);
+                    legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM); // căn dưới
+                    legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER); // căn giữa
+                    legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+                    legend.setDrawInside(false);
                     lineChart.setData(lineData);
                     lineChart.getDescription().setEnabled(false);
                     lineChart.getDescription().setTypeface(Typeface.DEFAULT_BOLD);
